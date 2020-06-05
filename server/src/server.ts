5912b69424af00e6e8934a0c6a1ebbed01ad6250
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2020-06-05 16:19:56 -0300

    rotas em arquivo separado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(routes)
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello World' })
+app.listen(3333, () => {
+  console.log('Servidor rodando na porta 3333')
 })
-
-app.listen(3333)
