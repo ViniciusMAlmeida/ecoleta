3817e45deee213583dabe960041ad172d8b99fbb
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2020-06-05 16:12:00 -0300

    limpando exemplos

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,28 +4,8 @@ const app = express()
 
 app.use(express.json())
 
-const users = ['joao', 'maria', 'jose']
-
 app.get('/users', (request, response) => {
-  const search = String(request.query.search)
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users
-
-  return response.json(filteredUsers)
-})
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const user = users[id]
-  return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-  const data = request.body
-
-  return response.json(data)
+  return response.json({ message: 'Hello World' })
 })
 
 app.listen(3333)
