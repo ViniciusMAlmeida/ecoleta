f2e35cecbcbfab37282c9c009fdcdd7ba5722831
ViniciusMAlmeida <vinicius.sec21@gmail.com>
2020-06-07 22:41:20 -0300

    manipulando estado do componente

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 import Header from './Header'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  function handleButtonClick() {
+    setCounter(counter + 1)
+  }
+
   return (
     <div>
-      <Header title="Hello World" />
-      <h1>Conteúdo da aplicação</h1>
+      <Header title={`Contador: ${counter}`} />
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>
+        Aumentar
+      </button>
     </div>
   )
 }
