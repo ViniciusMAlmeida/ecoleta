007e7c045374e4247692f1c142274c1a1abbcf56
ViniciusMAlmeida <vinicius.sec21@gmail.com>
2020-06-08 22:39:30 -0300

    submissão do formulário

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import { LeafletMouseEvent } from 'leaflet'
@@ -46,6 +46,8 @@ const CreatePoint = () => {
     0,
     0,
   ])
+
+  const history = useHistory()
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -112,7 +114,41 @@ const CreatePoint = () => {
   }
 
   function handleSelectItem(id: number) {
-    setSelectedItems([id])
+    const alreadySelected = selectedItems.findIndex((item) => item === id)
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id)
+      setSelectedItems(filteredItems)
+    } else {
+      setSelectedItems([...selectedItems, id])
+    }
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const { name, email, whatsapp } = formData
+    const uf = selectedUf
+    const city = selectedCity
+    const [latitude, longitude] = selectedPosition
+    const items = selectedItems
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items,
+    }
+
+    await api.post('points', data)
+
+    alert('Ponto de coleta criado!')
+
+    history.push('/')
   }
 
   return (
@@ -125,7 +161,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
